552d67d769664085f15025ddb7f047802332ac85
Savannah Sheppeard <caitlin_s_sheppeard@progressive.com>
2022-12-05 15:22:33 -0500

    practice

---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Take a break!");
+
+Console.WriteLine("How many minutes? :");
+string? minutes = Console.ReadLine();
+
+if (minutes is not null)
+{
+    DateUtils utils = new DateUtils();
+
+    int mins = int.Parse(minutes); // Could blow up!
+    DateTime timeAtEndOfBreak = utils.TakeABreak(mins);
+    Console.WriteLine($"Ok, be back at {timeAtEndOfBreak:T}"); 
+}
+else
+{
+    Console.WriteLine("Enter some minutes, moron");
+}
