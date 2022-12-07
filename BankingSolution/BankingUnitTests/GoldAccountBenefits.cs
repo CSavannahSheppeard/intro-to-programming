b615dd624c9ac575cfda2615824e51d6737d5e07
Savannah Sheppeard <caitlin_s_sheppeard@progressive.com>
2022-12-07 16:34:19 -0500

    EOD 12/7

--- BankingSolution/BankingUnitTests/GoldAccountBenefits.cs
+++ BankingSolution/BankingUnitTests/GoldAccountBenefits.cs
@@ -7,13 +7,19 @@ public class GoldAccountBenefits
     [Fact]
     public void GetBonusOnDeposit()
     {
+        // Given
+        var stubbedBonusCalculator = new Mock<ICalculateBonuses>();
         var account = new BankAccount(new StubbedBonusCalculator());
         var openingBalance = account.GetBalance();
         var amountToDeposit = 92.42M;
         var expectedBonus = 42M;
+        stubbedBonusCalculator.Setup(s => s.GetBonusForDepositOn(openingBalance, amountToDeposit)).Returns(expectedBonus));
 
+        // When
         account.Deposit(amountToDeposit);
 
+
+        // Then
         Assert.Equal(openingBalance + amountToDeposit + expectedBonus,
             account.GetBalance());
 
